63ea36034c087b611faac11c43793c88c35af552
Ivana Huckova <ivana.huckova@gmail.com>
2019-04-08 14:36:13 +0200

    Create enhancer succeed function tests and code

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,69 @@ module.exports = {
   get
 };
 
+//SUCCEED FUNCTION
 function succeed(item) {
-  return { ...item };
+  //typeof item, item.name, item.enhancement, item.durability
+  if (typeof item !== 'object') {
+    throw new Error('object required for item argument');
+  }
+  if (typeof item.name !== 'string') {
+    throw new Error('string required for item name');
+  }
+  if (typeof item.enhancement !== 'number') {
+    throw new Error('number required for item enhancement');
+  }
+  if (typeof item.durability !== 'number') {
+    throw new Error('number required for item durability');
+  }
+  if (!(item.name && item.durability && item.enhancement)) {
+    throw new Error('item name, durability and enhancement are required');
+  }
+  if (item.durability > 100) {
+    throw new Error('durability should not be larger than 100 ');
+  }
+  if (item.enhancement > 20) {
+    throw new Error('enhancement should not be larger than 20 ');
+  }
+  if (item.enhancement === 20) {
+    return { ...item };
+  }
+
+  const { enhancement } = item;
+  newEnhancement = enhancement + 1;
+  return { ...item, enhancement: newEnhancement };
 }
 
+//FAIL FUNCTION
 function fail(item) {
   return { ...item };
 }
 
+//REPAIR FUNCTION
 function repair(item) {
+  //typeof item, item.name, item.enhancement, item.durability
   if (typeof item !== 'object') {
     throw new Error('object required for item argument');
   }
+  if (typeof item.name !== 'string') {
+    throw new Error('string required for item name');
+  }
+  if (typeof item.enhancement !== 'number') {
+    throw new Error('number required for item enhancement');
+  }
+  if (typeof item.durability !== 'number') {
+    throw new Error('number required for item durability');
+  }
+  if (!(item.name && item.durability && item.enhancement)) {
+    throw new Error('item name, durability and enhancement are required');
+  }
+  if (item.durability > 100) {
+    throw new Error('durability should not be larger than 100 ');
+  }
+  if (item.enhancement > 20) {
+    throw new Error('enhancement should not be larger than 20 ');
+  }
+
   return { ...item, durability: 100 };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,12 +1,64 @@
 const enhancer = require('./enhancer.js');
 
 describe('enhancers module', () => {
+  //Repair function
   describe('repair function', () => {
     it('returns item object with durability 100', () => {
-      expect(enhancer.repair({ name: 'Hello', durability: 80 })).toEqual({ name: 'Hello', durability: 100 });
+      expect(enhancer.repair({ name: 'Hello', durability: 80, enhancement: 10 })).toEqual({ name: 'Hello', durability: 100, enhancement: 10 });
     });
+    //err
     it('throws error if item is not an object', () => {
-      expect(() => enhancer.repair(`durability`).toThrow());
+      expect(() => enhancer.repair(`item`).toThrow());
+    });
+    it('throws error if item does not have name, enhancement or durability', () => {
+      expect(() => enhancer.repair({ item }).toThrow());
+    });
+    it('throws error if item name is not a string', () => {
+      expect(() => enhancer.repair({ name: 70, durability: 10, enhancement: 10 }).toThrow());
+    });
+    it('throws error if item durability is not a number', () => {
+      expect(() => enhancer.repair({ name: 'John', durability: 'string', enhancement: 10 }).toThrow());
+    });
+    it('throws error if item enhancement is not a number', () => {
+      expect(() => enhancer.repair({ name: 'John', durability: 10, enhancement: 'string' }).toThrow());
+    });
+    it('throws error if item durability is larger than 100', () => {
+      expect(() => enhancer.repair({ name: 'John', durability: 110, enhancement: 10 }).toThrow());
+    });
+    it('throws error if item enhancement is larger than 20', () => {
+      expect(() => enhancer.repair({ name: 'John', durability: 100, enhancement: 25 }).toThrow());
+    });
+  });
+
+  //Succeed function
+  describe('succeed function', () => {
+    it('returns item object with enhancement + 1', () => {
+      expect(enhancer.succeed({ name: 'Hello', durability: 80, enhancement: 10 })).toEqual({ name: 'Hello', durability: 80, enhancement: 11 });
+    });
+    it('returns item object with enhancement 20 if previous state is 20', () => {
+      expect(enhancer.succeed({ name: 'Hello', durability: 80, enhancement: 20 })).toEqual({ name: 'Hello', durability: 80, enhancement: 20 });
+    });
+    //err
+    it('throws error if item is not an object', () => {
+      expect(() => enhancer.succeed(`item`).toThrow());
+    });
+    it('throws error if item does not have name, enhancement or durability', () => {
+      expect(() => enhancer.succeed({ item }).toThrow());
+    });
+    it('throws error if item name is not a string', () => {
+      expect(() => enhancer.succeed({ name: 70, durability: 10, enhancement: 10 }).toThrow());
+    });
+    it('throws error if item durability is not a number', () => {
+      expect(() => enhancer.succeed({ name: 'John', durability: 'string', enhancement: 10 }).toThrow());
+    });
+    it('throws error if item enhancement is not a number', () => {
+      expect(() => enhancer.succeed({ name: 'John', durability: 10, enhancement: 'string' }).toThrow());
+    });
+    it('throws error if item durability is larger than 100', () => {
+      expect(() => enhancer.succeed({ name: 'John', durability: 110, enhancement: 10 }).toThrow());
+    });
+    it('throws error if item enhancement is larger than 20', () => {
+      expect(() => enhancer.succeed({ name: 'John', durability: 100, enhancement: 25 }).toThrow());
     });
   });
 });
